09894a259e3b423754598674c9f9f624f4ecce27
premagikwad1213 <a70064182@gmail.com>
2026-01-05 02:11:55 +0530

    upate 20

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,4 +1,4 @@
-import { initTRPC } from '@trpc/server';
+import { initTRPC, TRPCError } from '@trpc/server';
 import {cache} from "react";
 import { auth } from '@clerk/nextjs/server';
 import superjson from "superjson";
@@ -17,6 +17,19 @@ const t = initTRPC.context<Context>().create({
    transformer: superjson,
 });
 // Base router and procedure helpers
-export const createTRPCrouter = t.router;
+export const createTRPCRouter = t.router;
 export const createCallerFactory  = t.createCallerFactory;
 export const baseprocedure = t.procedure;
+export const protectedProcedure = t.procedure.use(async function isAuthed(opts){
+    const {ctx} = opts;
+
+    if (!ctx.clerkUserId){
+        throw new TRPCError({code: "UNAUTHORIZED"});
+    }
+
+    return opts.next({
+        ctx:{
+            ...ctx,
+        }
+    })
+});
