32331f5f950f9292e358b19fa9126b92d98958d7
premagikwad1213 <a70064182@gmail.com>
2026-01-06 06:19:51 +0530

    new backend update

---
+++ src/modules/home/ui/views/home-view.tsx
@@ -0,0 +1,19 @@
+
+ import React from 'react'
+ 
+ interface HomeViewProps {
+    categoryId?:string;
+ }
+
+ const HomeView = ({
+    categoryId,
+ }: HomeViewProps) => {
+   return (
+     <div className="max-w-[2400px] mx-auto mb-10 px-4 pt-2.5 flex flex-col gap-y-6 ">
+       
+     </div>
+   )
+ }
+ 
+ export default HomeView
+ 
