9be0d32dfe1a29b919d1a1b58d353cadb8f2e4ad
premagikwad1213 <a70064182@gmail.com>
2026-01-14 22:48:57 +0530

    new update on studio

--- src/modules/videos/server/procedures.tsx
+++ src/modules/videos/server/procedures.tsx
@@ -1,14 +1,39 @@
 import { z } from "zod";
 import { db } from "@/db";
+import {and , eq} from "drizzle-orm"
 import { videos, videoUpdateSchema } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { mux } from "@/lib/mux";
+import { TRPCError } from "@trpc/server";
 
 export const videosRouter = createTRPCRouter({
   update: protectedProcedure
   .input(videoUpdateSchema)
   .mutation(async ({ctx, input}) => {
-      
+      const {id: userId} = ctx.user;
+
+      if (!input.id){
+         throw new TRPCError({code: "BAD_REQUEST"})
+      }
+
+      const [updatedVideo] = await db
+      .update(videos)
+      .set({
+        title: input.title,
+        description: input.description,
+        categoryId: input.categoryId,
+        visibility: input.visibility,
+        updatedAt: new Date(),
+      })
+      .where(and(
+        eq(videos.id, input.id),
+        eq(videos.userId, userId)
+      ))
+      .returning();
+
+      if(!updatedVideo){
+        throw new TRPCError({code: "NOT_FOUND"})
+      }
   }),
 
   create: protectedProcedure.mutation(async ({ ctx }) => {
