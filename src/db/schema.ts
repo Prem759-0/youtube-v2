bfde0ecd6682dfde4458114192a70ba066403e96
premagikwad1213 <a70064182@gmail.com>
2026-04-08 14:24:06 +0530

    comment update 4

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -9,6 +9,8 @@ import { z } from "zod";
 
 import {pgTable, text, uuid, integer, timestamp, uniqueIndex, pgEnum, primaryKey} from "drizzle-orm/pg-core"
 
+export const reactionType = pgEnum("reaction_type", ["like","dislike"]);
+
 export const users = pgTable("users", {
     id: uuid("id").primaryKey().defaultRandom(),
     clerkId: text("clerk_id").unique().notNull(),
@@ -31,6 +33,7 @@ export const userRelations = relations(users,({many})=>({
        relationName: "subscriptions_creator_id_fkey"
     }),
     comments:many(comments),
+    commentReactions: many(commentReactions),
 }))
 
 export const videoVisibility = pgEnum("video_visibility", [
@@ -129,7 +132,7 @@ export const comments = pgTable("comments", {
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
 })
 
-export const commentRelations = relations(comments, ({one})=>({
+export const commentRelations = relations(comments, ({one,many})=>({
     user: one(users,{
         fields: [comments.userId],
         references: [users.id],
@@ -138,11 +141,37 @@ export const commentRelations = relations(comments, ({one})=>({
         fields: [comments.videoId],
         references: [videos.id],
     }),
+    reactions: many(commentReactions),
 }))
 
 export const commentsInsertSchema = createInsertSchema( comments);
 export const commentsSelectSchema = createSelectSchema( comments);
 export const commentsUpdateSchema = createUpdateSchema( comments);
+
+export const commentReactions = pgTable("comment_reactions",{
+    userId: uuid("user_id").references(()=> users.id,{onDelete:"cascade"}).notNull(), 
+    commentId: uuid("comment_id").references(()=> comments.id,{onDelete:"cascade"}).notNull(), 
+    type: reactionType("type").notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+},(t)=>[
+    primaryKey({
+        name:"comment_reactions_pk",
+        columns:[t.userId,t.commentId],
+    }),
+]);
+
+export const commentReactionsRelations = relations(commentReactions, ({one})=>({
+    users: one(users, {
+        fields: [commentReactions.userId],
+        references:[users.id],
+    }),
+
+    comment: one(comments, {
+        fields: [commentReactions.commentId],
+        references:[comments.id],
+    })
+}))
 
 export const videoViews = pgTable("video_views",{
     userId: uuid("user_id").references(()=> users.id,{onDelete:"cascade"}).notNull(), 
@@ -157,12 +186,12 @@ export const videoViews = pgTable("video_views",{
 ]);
 
 export const videoViewRelations = relations(videoViews, ({one})=>({
-    users: one(users, {
+    user: one(users, {
         fields: [videoViews.userId],
         references:[users.id],
     }),
 
-    videos: one(videos, {
+    video: one(videos, {
         fields: [videoViews.videoId],
         references:[videos.id],
     })
@@ -172,7 +201,7 @@ export const videoViewSelectSchema = createSelectSchema(videoViews);
 export const videoViewInsertSchema = createInsertSchema(videoViews);
 export const videoViewUpdateSchema = createUpdateSchema(videoViews);
 
-export const reactionType = pgEnum("reaction_type", ["like","dislike"]);
+
 
 export const videoReactions = pgTable("video_reactions",{
     userId: uuid("user_id").references(()=> users.id,{onDelete:"cascade"}).notNull(), 
@@ -188,12 +217,12 @@ export const videoReactions = pgTable("video_reactions",{
 ]);
 
 export const videoReactionsRelations = relations(videoReactions, ({one})=>({
-    users: one(users, {
+    user: one(users, {
         fields: [videoReactions.userId],
         references:[users.id],
     }),
 
-    videos: one(videos, {
+    video: one(videos, {
         fields: [videoReactions.videoId],
         references:[videos.id],
     })
--- src/modules/comments/ui/components/comment-item.tsx
+++ src/modules/comments/ui/components/comment-item.tsx
@@ -1,22 +1,41 @@
 import Link from "next/link";
 import { CommentsGetManyOutput } from "../../types";
 import { UserAvatar } from "@/components/user-avatar";
+import {toast} from "sonner"
 import { formatDistanceToNow } from "date-fns";
 import { trpc } from "@/trpc/client";
+import {Button} from "@/components/ui/button";
 import {
     DropdownMenu,
     DropdownMenuItem,
     DropdownMenuTrigger,
     DropdownMenuContent,
 } from "@/components/ui/dropdown-menu"
+import { MessageSquareIcon, MoreVerticalIcon, Trash2Icon } from "lucide-react";
+import { useAuth, useClerk } from "@clerk/nextjs";
 
 interface CommentItemProps {
     comment: CommentsGetManyOutput["items"][number];
 };
 
 export const CommentItem = ({ comment }: CommentItemProps) =>{
+    const clerk = useClerk();
+    const {userId} =  useAuth(); 
 
-    const remove = trpc.comments.remove.useMutation();
+    const utils = trpc.useUtils()
+    const remove = trpc.comments.remove.useMutation({
+        onSuccess: ()  => {
+            toast.success("Comment deleted successfully ✅");
+            utils.comments.getMany.invalidate({videoId: comment.videoId})
+        },
+        onError: (error)  => {
+            toast.error("Failed to delete comment ‼️");
+
+            if (error.data?.code === "UNAUTHORIZED"){
+                clerk.openSignIn();
+            }
+        },
+    });
 
     return(
         <div>
@@ -42,7 +61,27 @@ export const CommentItem = ({ comment }: CommentItemProps) =>{
                       </div>
                    </Link>
                    <p className="text-sm">{comment.value}</p>
+
                 </div>
+                <DropdownMenu modal={false}>
+                    <DropdownMenuTrigger asChild >
+                        <Button variant="ghost" size="icon" className="size-8">
+                            <MoreVerticalIcon/>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuItem onClick={()=>{}}>
+                            <MessageSquareIcon className="size-4 "/>
+                            Reply
+                        </DropdownMenuItem>
+                        {comment.user.clerkId === userId && (
+                        <DropdownMenuItem onClick={()=> remove.mutate({id: comment.id})}>
+                            <Trash2Icon className="size-4 "/>
+                            Delete
+                        </DropdownMenuItem>
+                        )}
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </div>
         </div>
     )
