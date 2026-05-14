c5f7c1d538de6ca85afaa05eb6694b610fb60ee4
premagikwad1213 <a70064182@gmail.com>
2026-05-14 19:00:15 +0530

    update

--- src/modules/videos/ui/sections/suggestion-sections.tsx
+++ src/modules/videos/ui/sections/suggestion-sections.tsx
@@ -4,15 +4,18 @@ import { DEFAULT_LIMIT } from "@/constants";
 import { trpc } from "@/trpc/client";
 import { VideoRowCard } from "../components/video-row-card";
 import { VideoGridCard } from "../components/video-grid-card";
+import { InfiniteScroll } from "@/components/infinite-scroll";
 
 interface SuggestionsSectionProps {
     videoId: string;
+    isManual?: boolean;
 }
 
 export const SuggestionsSection = ({
     videoId,
+    isManual,
 }: SuggestionsSectionProps) => {
-    const [suggestions] = trpc.suggestions.getMany.useSuspenseInfiniteQuery({
+    const [suggestions, query] = trpc.suggestions.getMany.useSuspenseInfiniteQuery({
         videoId,
         limit: DEFAULT_LIMIT,
     },{
@@ -33,6 +36,11 @@ export const SuggestionsSection = ({
                 <VideoGridCard key={video.id} data={video}  />
             ))}
         </div>
+        <InfiniteScroll
+          hasNextPage={query.hasNextPage}
+          isFetchingNextPage={query.isFetchingNextPage}
+          fetchNextPage={query.fetchNextPage}
+        />
         </>
     )
 }
