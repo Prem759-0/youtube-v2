6ee95e208a039dc60228a9308b45a247de89553e
prem-web.dev <a70064182@gmail.com>
2026-07-05 08:40:39 +0530

    Fix current user profile redirect route

--- src/app/(home)/users/current/route.ts
+++ src/app/(home)/users/current/route.ts
@@ -1,7 +1,6 @@
-import { NextResponse } from 'next/server';
-
 import { auth } from '@clerk/nextjs/server';
 import { eq } from 'drizzle-orm';
+import { NextResponse } from 'next/server';
 
 import { db } from '@/db';
 import { users } from '@/db/schema';
@@ -10,11 +9,18 @@ import { absoluteUrl } from '@/lib/utils';
 export const GET = async () => {
 	const { userId } = await auth();
 
-	if (!userId) return NextResponse.redirect(absoluteUrl('/sign-in'));
+	if (!userId) {
+		return NextResponse.redirect(absoluteUrl('/sign-in'));
+	}
 
-	const [existingUser] = await db.select().from(users).where(eq(users.clerkId, userId));
+	const [existingUser] = await db
+		.select({ id: users.id })
+		.from(users)
+		.where(eq(users.clerkId, userId));
 
-	if (!existingUser) return NextResponse.redirect(absoluteUrl('/sign-in'));
+	if (!existingUser) {
+		return NextResponse.redirect(absoluteUrl('/sign-in'));
+	}
 
 	return NextResponse.redirect(absoluteUrl(`/users/${existingUser.id}`));
 };
